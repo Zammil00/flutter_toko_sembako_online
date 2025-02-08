caef2e840f15c4c9515a5c577069806171d1814e
INI_LABIL <115417258+Zammil00@users.noreply.github.com>
2025-02-09 00:32:27 +0700

    Create Splash Page

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import 'package:toko_sembako_online/pages/home_page.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    Future.delayed(
+      Duration(seconds: 3),
+      () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ),
+        );
+      },
+    );
+    return Scaffold(
+      backgroundColor: Colors.deepOrange,
+      body: Center(
+        child: Lottie.asset(
+          'assets/animations/splash.json',
+          width: 400,
+          height: 400,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
   }
 }
