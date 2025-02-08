11cdb7f55dc0465922408b7b668dd1837158353a
INI_LABIL <115417258+Zammil00@users.noreply.github.com>
2025-02-08 22:35:27 +0700

    Update List Produk View

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:toko_sembako_online/models/data/data.dart';
+import 'package:toko_sembako_online/pages/list_produk.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,181 +39,59 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 1,
-        backgroundColor: Colors.white,
-        leading: Icon(
-          Icons.menu,
-          color: Colors.black,
-        ),
-        title: Text(
-          "TOKO SEMBAKO",
-          style: GoogleFonts.lato(
+        appBar: AppBar(
+          elevation: 1,
+          backgroundColor: Colors.white,
+          leading: Icon(
+            Icons.menu,
             color: Colors.black,
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
           ),
-        ),
-        actions: [
-          Row(
-            children: [
-              Icon(
-                Icons.search,
-              ),
-              Stack(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.shopping_cart,
-                    ),
-                  ),
-                  Positioned(
-                    top: 0,
-                    right: 3,
-                    child: Container(
-                      height: 20,
-                      width: 20,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.deepOrangeAccent,
+          title: Text(
+            "TOKO SEMBAKO",
+            style: GoogleFonts.lato(
+              color: Colors.black,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          actions: [
+            Row(
+              children: [
+                Icon(
+                  Icons.search,
+                ),
+                Stack(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.shopping_cart,
                       ),
-                      child: Center(
-                        child: Text(
-                          "2",
-                          style: GoogleFonts.lato(color: Colors.white),
+                    ),
+                    Positioned(
+                      top: 0,
+                      right: 3,
+                      child: Container(
+                        height: 20,
+                        width: 20,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.deepOrangeAccent,
+                        ),
+                        child: Center(
+                          child: Text(
+                            "2",
+                            style: GoogleFonts.lato(color: Colors.white),
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                ],
-              )
-            ],
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.all(10),
-        child: GridView.count(
-          mainAxisSpacing: 10,
-          crossAxisSpacing: 10,
-          crossAxisCount: 2,
-          childAspectRatio: 0.6,
-          children: [
-            Card(
-              elevation: 2,
-              shadowColor: Colors.lightGreen,
-              color: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Hero(
-                    tag: dataCard.gambar,
-                    child: SizedBox(
-                      width: 150,
-                      child: Image.asset(dataCard.gambar),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 6,
-                  ),
-                  Text(
-                    "Rp " + dataCard.harga,
-                    style: GoogleFonts.lato(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 2,
-                  ),
-                  Text(
-                    dataCard.nama,
-                    style: GoogleFonts.lato(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 2,
-                  ),
-                  Text(
-                    dataCard.jumlah,
-                    style: GoogleFonts.lato(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w300,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 4,
-                  ),
-                  Divider(
-                    height: 2,
-                    color: Colors.deepOrangeAccent,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.add_shopping_cart,
-                            size: 20,
-                            color: Colors.deepOrangeAccent,
-                          ),
-                          SizedBox(
-                            width: 2,
-                          ),
-                          Text(
-                            "Beli",
-                            style: GoogleFonts.lato(
-                              color: Colors.deepOrangeAccent,
-                              fontSize: 16,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.remove_circle_outline,
-                                size: 18,
-                                color: Colors.deepOrangeAccent,
-                              ),
-                              Padding(
-                                padding: EdgeInsets.symmetric(
-                                  horizontal: 5,
-                                ),
-                                child: Text("0"),
-                              ),
-                              Icon(
-                                Icons.add_circle_outline,
-                                size: 18,
-                                color: Colors.deepOrangeAccent,
-                              )
-                            ],
-                          )
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-            Card(),
+                    )
+                  ],
+                )
+              ],
+            )
           ],
         ),
-      ),
-    );
+        body: ListProduk());
   }
 }
--- lib/models/data/data.dart
+++ lib/models/data/data.dart
@@ -13,3 +13,55 @@ class Produk {
     this.deskripsi,
   );
 }
+
+final List<Produk> produkList = [
+  Produk(
+    "Telur",
+    "10.000",
+    "Butir",
+    "assets/images/g2.jpg",
+    "Telur Hasil Peternakan Kampung Aceh",
+  ),
+  Produk(
+    "Beras",
+    "50.000",
+    "Kg",
+    "assets/images/g3.jpg",
+    "Beras premium hasil panen terbaik",
+  ),
+  Produk(
+    "Gula",
+    "15.000",
+    "Kg",
+    "assets/images/g4.jpg",
+    "Gula pasir berkualitas tinggi",
+  ),
+  Produk(
+    "Minyak Goreng",
+    "20.000",
+    "Liter",
+    "assets/images/g5.jpg",
+    "Minyak goreng sehat untuk keluarga",
+  ),
+  Produk(
+    "Susu",
+    "25.000",
+    "Kotak",
+    "assets/images/g6.jpg",
+    "Susu murni dari sapi pilihan",
+  ),
+  Produk(
+    "Kopi",
+    "30.000",
+    "Bungkus",
+    "assets/images/g7.jpg",
+    "Kopi robusta asli Aceh",
+  ),
+  Produk(
+    "Tepung Terigu",
+    "12.000",
+    "Kg",
+    "assets/images/g8.jpg",
+    "Tepung terigu berkualitas tinggi",
+  ),
+];
