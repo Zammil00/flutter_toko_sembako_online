6fdcd0bf850ba42281a43202f2ce443b115f0706
INI_LABIL <115417258+Zammil00@users.noreply.github.com>
2025-02-08 21:59:13 +0700

    Create Data Class

---
+++ lib/models/data/data.dart
@@ -0,0 +1,15 @@
+class Produk {
+  final String nama;
+  final String harga;
+  final String jumlah;
+  final String gambar;
+  final String deskripsi;
+
+  Produk(
+    this.nama,
+    this.harga,
+    this.jumlah,
+    this.gambar,
+    this.deskripsi,
+  );
+}
